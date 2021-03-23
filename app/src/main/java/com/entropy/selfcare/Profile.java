c6d2cb3dd777f8c87d65b0bef4b5d906edc4f373
nandhini093 <nandhini9320@gmail.com>
2021-03-23 18:00:03 +0530

    changed backround color

--- app/src/main/java/com/entropy/selfcare/Profile.java
+++ app/src/main/java/com/entropy/selfcare/Profile.java
@@ -1,14 +1,44 @@
 package com.entropy.selfcare;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.ResourceBundle;
 
 public class Profile extends AppCompatActivity {
+    TextView fullName, email;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fstore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        fullName = findViewById(R.id.txtProfileName);
+        email = findViewById(R.id.txtProfileEmail);
+
+        fAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
+
+        userID = fAuth.getCurrentUser().getUid();
+
+        DocumentReference documentReference = fstore.collection("users").document(userID);
+        documentReference.addSnapshotListener(this, (documentSnapshot, e) -> {
+            fullName.setText(documentSnapshot.getString("fname"));
+            email.setText(documentSnapshot.getString("email"));
+        });
+
     }
 }
+
--- app/src/main/java/com/entropy/selfcare/Register.java
+++ app/src/main/java/com/entropy/selfcare/Register.java
@@ -1,3 +1,4 @@
+
 package com.entropy.selfcare;
 
 import androidx.annotation.NonNull;
@@ -7,6 +8,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,17 +17,26 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.gson.internal.$Gson$Preconditions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
     FirebaseAuth fAuth;
     TextView textView;
     Button button;
-    EditText mName,mEmail,mPass;
+    EditText mFullName,mEmail,mPass;
     ImageView imageView;
+    FirebaseFirestore fstore;
+    String userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,23 +48,25 @@ public class Register extends AppCompatActivity {
         getSupportActionBar().setTitle("Register");
 
         button = (Button) findViewById(R.id.btnLetsGo);
-        mName = (EditText) findViewById(R.id.edtNewUser);
+        mFullName = (EditText) findViewById(R.id.edtNewUser);
         mPass = (EditText) findViewById(R.id.edtNewPass);
         mEmail = (EditText) findViewById(R.id.edtEmail);
         imageView = (ImageView) findViewById(R.id.imgRegister);
 
         fAuth = FirebaseAuth.getInstance();
+        fstore = FirebaseFirestore.getInstance();
 
         if (fAuth.getCurrentUser() != null){
             startActivity(new Intent(getApplicationContext(),HomePage.class));
             finish();
         }
-
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = mEmail.getText().toString().trim();
                 String password = mPass.getText().toString().trim();
+                String fullName = mFullName.getText().toString();
+
 
                 if(TextUtils.isEmpty(email)){
                     mEmail.setError("Email is required");
@@ -73,6 +86,17 @@ public class Register extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(task.isSuccessful()){
                             Toast.makeText(Register.this,"User created",Toast.LENGTH_SHORT).show();
+                            userID = fAuth.getCurrentUser().getUid();
+                            DocumentReference documentReference = fstore.collection("users").document(userID);
+                            Map<String,Object> user = new HashMap<>();
+                            user.put("fname",fullName);
+                            user.put("email",email);
+                            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("TAG","onSuccess: user profile is created: " +userID);
+                                }
+                            });
                             startActivity(new Intent(getApplicationContext(),HomePage.class));
                         }else{
                             Toast.makeText(Register.this,"Error has occurred"+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
@@ -85,3 +109,4 @@ public class Register extends AppCompatActivity {
     }
 
 }
+
